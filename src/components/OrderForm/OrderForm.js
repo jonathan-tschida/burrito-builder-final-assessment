ad4e72874a1702e2a3d712380b0ba402bbc10685
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-22 11:32:44 -0600

    Add logic to change styling of ingredient buttons when they are selected

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './OrderForm.css';
 import { connect } from 'react-redux';
 import { addOrder } from '../../actions';
 import { postOrder } from '../../apiCalls';
@@ -43,7 +44,12 @@ class OrderForm extends Component {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button
+          key={ingredient}
+          name={ingredient}
+          className={this.state.ingredients.includes(ingredient) ? 'selected' : ''}
+          onClick={e => this.handleIngredientChange(e)}
+        >
           {ingredient}
         </button>
       )
