38ca858eb2302032a2b51f381dbcfb203de4dec5
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-22 10:46:29 -0600

    Add tests for orders reducer

---
+++ src/reducers/orders.test.js
@@ -0,0 +1,34 @@
+import { orders } from './orders';
+
+describe('Orders Reducer', () => {
+  it('should set the orders to a given array when receiving SET_ORDERS', () => {
+    const testOrders = [
+      {id: 1, name: 'Bob', ingredients: ['beans', 'steak']},
+      {id: 2, name: 'Bill', ingredients: ['lettuce', 'sofritas']}
+    ];
+    const action = {
+      type: 'SET_ORDERS',
+      orders: testOrders
+    };
+    const expectedResult = testOrders;
+    const result = orders([], action);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should add an order to state when receiving ADD_ORDER', () => {
+    const previousState = [
+      {id: 1, name: 'Bob', ingredients: ['beans', 'steak']}
+    ];
+    const newOrder = {id: 2, name: 'Bill', ingredients: ['lettuce', 'sofritas']}
+    const action = {
+      type: 'ADD_ORDER',
+      order: newOrder
+    };
+    const expectedResult = [
+      {id: 1, name: 'Bob', ingredients: ['beans', 'steak']},
+      {id: 2, name: 'Bill', ingredients: ['lettuce', 'sofritas']}
+    ];
+    const result = orders(previousState, action);
+    expect(result).toEqual(expectedResult);
+  });
+});
