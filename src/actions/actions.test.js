39e1d14650a095601e55864907dc1a5a6cbc09ea
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-22 10:46:10 -0600

    Add tests for action creators

---
+++ src/actions/actions.test.js
@@ -0,0 +1,26 @@
+import * as actions from '.';
+
+describe('Action Creators', () => {
+  it('should have a type of SET_ORDERS and a correct payload', () => {
+    const orders = [
+      {id: 1, name: 'Bob', ingredients: ['beans', 'steak']},
+      {id: 2, name: 'Bill', ingredients: ['lettuce', 'sofritas']}
+    ];
+    const expectedResult = {
+      type: 'SET_ORDERS',
+      orders
+    };
+    const result = actions.setOrders(orders);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should have a type of ADD_ORDER and a correct payload', () => {
+    const order = {id: 1, name: 'Bob', ingredients: ['beans', 'steak']};
+    const expectedResult = {
+      type: 'ADD_ORDER',
+      order
+    };
+    const result = actions.addOrder(order);
+    expect(result).toEqual(expectedResult);
+  });
+});
